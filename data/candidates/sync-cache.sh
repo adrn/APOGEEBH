2898b8c906bb68cddd8761422e1e7fe2d2b76ff9
Adrian Price-Whelan <adrian.prw@gmail.com>
2018-03-24 21:12:48 -0400

    skip chain

--- data/candidates/sync-cache.sh
+++ data/candidates/sync-cache.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rsync -zvr --max-size=1000m --exclude "*~" \
+rsync -zvr --max-size=1000m --exclude "*~" --exclude "*.npy" \
 perseus:/tigress/adrianp/projects/apogeebh/data/candidates/cache ~/projects/apogeebh/data/candidates
