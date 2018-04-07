2391512b4f6f382a8b8d12fdac35a235360415fe
Adrian Price-Whelan <adrian.prw@gmail.com>
2018-04-07 16:51:53 -0400

    mpi scripts

--- mpi/load-db.sh
+++ mpi/load-db.sh
@@ -2,20 +2,18 @@
 #SBATCH -J apogeebh          # job name
 #SBATCH -o apogeebh.o%j             # output file name (%j expands to jobID)
 #SBATCH -e apogeebh.e%j             # error file name (%j expands to jobID)
-#SBATCH -n 448
-#SBATCH -t 24:00:00             # run time (hh:mm:ss) - 1.5 hours
+#SBATCH -n 1
+#SBATCH -t 06:00:00             # run time (hh:mm:ss) - 1.5 hours
 #SBATCH --mail-user=adrn@princeton.edu
 #SBATCH --mail-type=begin       # email me when the job starts
 #SBATCH --mail-type=end         # email me when the job finishes
 
 cd /tigress/adrianp/projects/apogeebh/scripts/
 
-module load openmpi/gcc/1.10.2/64
-
 source activate twoface
 
 date
 
-srun python run_joker.py -v --mpi
+python load_dr15_db.py --db=../cache/apogeebh.sqlite \ --allstar=../apogeebh/db/tests/allStar.fits \ --allvisit=../apogeebh/db/tests/allVisit.fits -o
 
 date
